b49c065b20079bd3430cff698f11a305cd7ce7dc
aidanlaw <law.aidan@gmail.com>
2014-04-23 22:34:07 +0800

    Initial Commit

---
+++ 2008 - Semester 2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  2008 - Semester 2
+//
+//  Created by Aidan Law on 23/04/2014.
+//  Copyright (c) 2014 Aidan Law. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
